3788596a7d8ea55a29f2f7fd8d6b552e22c3a1b8
贺鹏飞 <hepengfei@chuangjiangx.com>
2017-11-06 11:24:35 +0800

    change to WarpCTC

--- detect.py
+++ detect.py
@@ -26,7 +26,7 @@ def detect(test_inputs, test_targets, test_seq_len):
     with tf.Session() as sess:
         # Restore variables from disk.
         # saver.restore(sess, "models/ocr.model-0.95-94999")
-        saver.restore(sess, "models/ocr.model-3000.data-00000-of-00001")
+        saver.restore(sess, "models/ocr.model-3000")
         print("Model restored.")
         # feed_dict = {inputs: test_inputs, targets: test_targets, seq_len: test_seq_len}
         feed_dict = {inputs: test_inputs, seq_len: test_seq_len}
